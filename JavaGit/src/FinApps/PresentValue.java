47f09060680d7bc7c48d318539b67ad5acb69eda
Muddu <Muddu@192.168.69.32>
2017-06-17 19:41:56 +0530

    Spreads updated

--- JavaGit/src/FinApps/PresentValue.java
+++ JavaGit/src/FinApps/PresentValue.java
@@ -2,47 +2,57 @@
  * Project developed for Financial Engineering Math package
  * Reference: Java methods for Financial engineering @Philip/ Barker
  */
-
 package FinApps;
+
 /**
  *
  * @author Muddu
  */
-public class PresentValue 
-{
-    /**Instance of the class**/
-    public PresentValue()
-    {
+public class PresentValue {
+
+    /**
+     * Instance of the class*
+     */
+    public PresentValue() {
     }
-    public double pV(double[] discounts,double[] cashflows)
-    {
-        double presval=0;
-        for(int i=0;i<cashflows.length;i++)//returns sum of discounted values
+
+    public double pV(double[] discounts, double[] cashflows) {
+        double presval = 0;
+        for (int i = 0; i < cashflows.length; i++)//returns sum of discounted values
         {
-           presval+=discounts[i]*cashflows[i]; 
+            presval += discounts[i] * cashflows[i];
         }
         return presval;
     }
-    public double pV(double r,double[] cashflows)//implements PV for given IR
+
+    public double pV(double r, double[] cashflows)//implements PV for given IR
     {
-        int indx=1;
-        double sum=0;
-        for(int i=0;i<cashflows.length;i++)
-        {
-            sum+=(cashflows[i]/Math.pow((1+r),indx));
+        int indx = 1;
+        double sum = 0;
+        for (int i = 0; i < cashflows.length; i++) {
+            sum += (cashflows[i] / Math.pow((1 + r), indx));
             indx++;
         }
         return sum;
     }
-    public double pV(double r,double cash,int period)
-    {
-        double sum=0;
-        int indx=1;
-        for(int i=0;i<period;i++)
-        {
-            sum+=(cash/Math.pow(1+r,indx));
+
+    public double pV(double r, double cash, int period) {
+        double sum = 0;
+        int indx = 1;
+        for (int i = 0; i < period; i++) {
+            sum += (cash / Math.pow(1 + r, indx));
             indx++;
         }
         return sum;
+    }
+
+    public static double pVonecash(double[] discounts, double cashflow) {
+        int i = 0;
+        double pv = 0;
+        for (double d : discounts) {
+            pv += (Math.exp(-(i + 1) * Math.log(1.0 + (d / 100.0))) * cashflow);
+            i++;
+        }
+        return pv;
     }
 }
