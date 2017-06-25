e97402e3961e044db215abf1b8456bf9c947db67
Muddu <Muddu@192.168.69.32>
2017-06-25 15:11:38 +0530

    Updated with Options main classes

--- JavaGit/src/FinApps/Forwards.java
+++ JavaGit/src/FinApps/Forwards.java
@@ -12,26 +12,25 @@ public class Forwards {
 
     public Forwards() {
     }
+
     public static double dollarIntr(double term, double reporate) {
         return reporate * (term / 360.0);
     }
+
     public static double delpriceNoinc(double spotprice, double maturity, double reporate) {
         return (spotprice * conintr(reporate, maturity));
     }
 
     private static double conintr(double reporate, double maturity) {
-        return Math.exp(maturity*reporate);
+        return Math.exp(maturity * reporate);
     }
 
-    public static double fpriceNoinc(double spotprice, double maturity,
-            double currentime, double deliveryprice, double reporate) {
-
+    public static double fpriceNoinc(double spotprice, double maturity, double currentime, double deliveryprice, double reporate) {
         return (spotprice - (PresentValue.pVcont(reporate, (maturity - currentime),
                 deliveryprice)));
     }
 
-    public static double fpriceInc(double spotprice, double maturity,
-            double currentime, double reporate, double period, double dividend) {
+    public static double fpriceInc(double spotprice, double maturity, double currentime, double reporate, double period, double dividend) {
         double income = 0.0;
         income = maturity == 1.0 ? PresentValue.pVcont(reporate, 1.0, dividend) : 0.0;
         double limit = 0.0;
@@ -46,7 +45,7 @@ public class Forwards {
                 (maturity - currentime))));
     }
 
-    public static double fpriceInc(double spotprice, double maturity,double currentime,double[] reporate, double period, double dividend) {
+    public static double fpriceInc(double spotprice, double maturity, double currentime, double[] reporate, double period, double dividend) {
         double income = 0.0;
         double limit = 0.0;
         double forwardprice = 0.0;
@@ -62,7 +61,7 @@ public class Forwards {
 
     public static double fvalueInc(double spotprice, double maturity, double currentime, double reporate, double period, double dividend, double deliveryprice) {
         double income = 0.0;
-        income = maturity == 1.0 ? PresentValue.pVcont(reporate, 1.0, dividend): 0.0;
+        income = maturity == 1.0 ? PresentValue.pVcont(reporate, 1.0, dividend) : 0.0;
         double limit = 0.0;
         limit = (maturity - currentime);
         double time = (period / 12.0);
@@ -74,7 +73,7 @@ public class Forwards {
         return ((spotprice - income) - (deliveryprice * PresentValue.pVcont(reporate, (maturity - currentime))));
     }
 
-    public static double fvalueInc(double spotprice, double maturity, double currentime, double[] reporate, double period,double dividend, double deliveryprice) {
+    public static double fvalueInc(double spotprice, double maturity, double currentime, double[] reporate, double period, double dividend, double deliveryprice) {
         double income = 0.0;
         double forwardprice = 0.0;
         double time = (period / 12.0);
@@ -87,16 +86,16 @@ public class Forwards {
     }
     // Also parity rate calculation
 
-    public static double fvaluegen(double fprice, double delivprice, double maturity,double currentime, double reporate) {
-        return ((fprice - delivprice) * PresentValue.pVcont(reporate,(maturity - currentime)));
+    public static double fvaluegen(double fprice, double delivprice, double maturity, double currentime, double reporate) {
+        return ((fprice - delivprice) * PresentValue.pVcont(reporate, (maturity - currentime)));
     }
 
-    public static double fpriceDyld(double spotprice,double maturity, double currentime, double reporate, double dividendyld) {
-        return (spotprice * conintr((reporate - dividendyld),(maturity - currentime)));
+    public static double fpriceDyld(double spotprice, double maturity, double currentime, double reporate, double dividendyld) {
+        return (spotprice * conintr((reporate - dividendyld), (maturity - currentime)));
     }
 
-    public static double fvalueDyld(double spotprice, double maturity, double currentime, double reporate, double dividendyld,double deliveryprice) {
-        return ((fpriceDyld(spotprice, maturity, currentime,reporate, dividendyld) - (deliveryprice)) * PresentValue.pVcont(reporate,(maturity - currentime)));
+    public static double fvalueDyld(double spotprice, double maturity, double currentime, double reporate, double dividendyld, double deliveryprice) {
+        return ((fpriceDyld(spotprice, maturity, currentime, reporate, dividendyld) - (deliveryprice)) * PresentValue.pVcont(reporate, (maturity - currentime)));
     }
 
 }
